5a614a2c7be42537b797ebc6a40a83ccc307bd2a
jhs512 <jangka512@gmail.com>
2021-11-14 22:14:50 +0900

    기본 마이그레이트, mysql 연결

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'sbsst',
+        'PASSWORD': 'sbs123414',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
