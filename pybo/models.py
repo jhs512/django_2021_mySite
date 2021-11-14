a992ddbdd0a7900f8f73a9dea58414786a9aafb6
jhs512 <jangka512@gmail.com>
2021-11-14 22:44:36 +0900

    question, answer 테이블 생성

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField(default='')
+    create_date = models.DateTimeField(default=None)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, default=None)
+    content = models.TextField(default='')
+    create_date = models.DateTimeField(default=None)
